e9f803f4bf62295ff36f5f72c1e6da9436fe5bd9
raji808 <rajeshwari.v@stat.christuniversity.in>
2025-07-21 12:51:47 +0530

    Sync Changes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -2,9 +2,20 @@
 
 return [
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_DRIVER', 'pusher'),
 
     'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+            ],
+        ],
 
         'ably' => [
             'driver' => 'ably',
